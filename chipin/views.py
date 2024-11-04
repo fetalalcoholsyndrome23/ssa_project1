991a26c5137dc0c220eef92deb846f0892a97a4e
Daniel Oh <danieloh@daniu.local>
2024-11-05 10:07:06 +1100

    Refactor Invitation Email Notification

--- chipin/views.py
+++ chipin/views.py
@@ -60,3 +60,21 @@ def invite_users(request, group_id):
         'group': group,
         'users_not_in_group': users_not_in_group
     })
+
+@login_required
+def invite_users(request, group_id):
+    group = get_object_or_404(Group, id=group_id)
+    users_not_in_group = User.objects.exclude(id__in=group.members.values_list('id', flat=True))
+    if request.method == 'POST':
+        user_id = request.POST.get('user_id')
+        invited_user = get_object_or_404(User, id=user_id)      
+        if invited_user in group.invited_users.all():
+            messages.info(request, f'{invited_user.username} has already been invited.')
+        else:
+            group.invited_users.add(invited_user)
+            messages.success(request, f'Invitation sent to {invited_user.username}.')
+        return redirect('chipin:group_detail', group_id=group.id)  
+    return render(request, 'chipin/invite_users.html', {
+        'group': group,
+        'users_not_in_group': users_not_in_group
+    })
