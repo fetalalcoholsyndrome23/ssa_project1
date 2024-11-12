71cb23d0f4020a662883ee9d2279a154cb89639e
Daniel Oh <danieloh@daniu.local>
2024-11-12 12:16:47 +1100

    Working week 4?

--- chipin/views.py
+++ chipin/views.py
@@ -8,13 +8,13 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from .forms import GroupCreationForm
 from .models import Group
+from .models import GroupJoinRequest
 import urllib.parse
 
 @login_required
 def home(request):
     pending_invitations = Group.objects.filter(invited_users__email=request.user.email)
     return render(request, "chipin/home.html", {'pending_invitations': pending_invitations})
-
 
 @login_required
 def create_group(request):
@@ -42,24 +42,6 @@ def delete_group(request, group_id):
     else:
         messages.error(request, "You do not have permission to delete this group.")
     return redirect('chipin:home')
-
-@login_required
-def invite_users(request, group_id):
-    group = get_object_or_404(Group, id=group_id)
-    users_not_in_group = User.objects.exclude(id__in=group.members.values_list('id', flat=True))
-    if request.method == 'POST':
-        user_id = request.POST.get('user_id')
-        invited_user = get_object_or_404(User, id=user_id)      
-        if invited_user in group.invited_users.all():
-            messages.info(request, f'{invited_user.username} has already been invited.')
-        else:
-            group.invited_users.add(invited_user)
-            messages.success(request, f'Invitation sent to {invited_user.username}.')
-        return redirect('chipin:group_detail', group_id=group.id)  
-    return render(request, 'chipin/invite_users.html', {
-        'group': group,
-        'users_not_in_group': users_not_in_group
-    })
 
 @login_required
 def invite_users(request, group_id):
@@ -97,25 +79,80 @@ def accept_invite(request, group_id):
         messages.error(request, "Invalid invitation link.")  
     return redirect('chipin:group_detail', group_id=group.id)
 
-def send_invitation_email(group, email, request):
-    encoded_email = urllib.parse.quote(email)
-    invite_url = request.build_absolute_uri(reverse('chipin:accept_invite', args=[group.id])) + f"?email={encoded_email}"
-    subject = f"You have been invited to join the group {group.name}"
-    message = (
-        f"""
-You have been invited to join the group {group.name}.
+@login_required
+def home(request):
+    # Get all groups where the user has been invited but not accepted the invite
+    pending_invitations = Group.objects.filter(invited_users=request.user)
+    
+    # Get all join requests submitted by the current user
+    user_join_requests = GroupJoinRequest.objects.filter(user=request.user)
 
-"""
-        f"""
-OK
+    # Get all groups where the user is NOT a member
+    available_groups = Group.objects.exclude(members=request.user)
 
-"""
-    )
+    return render(request, 'chipin/home.html', { # Pass data to the template
+        'pending_invitations': pending_invitations, 
+        'user_join_requests': user_join_requests, 
+        'available_groups': available_groups  
+    })
 
-    send_mail(
-        subject,
-        '',  # No plain text body, only HTML
-        f"ChipIn ",
-        [email],
-        html_message=message,  # The HTML message
-    )
+@login_required
+def request_to_join_group(request, group_id):
+    group = get_object_or_404(Group, id=group_id)
+    # Check if the user is already a member
+    if request.user in group.members.all():
+        messages.info(request, "You are already a member of this group.")
+        return redirect('chipin:group_detail', group_id=group.id)
+    # Check if the user has already submitted a join request
+    join_request, created = GroupJoinRequest.objects.get_or_create(user=request.user, group=group)
+    if created:
+        messages.success(request, "Your request to join the group has been submitted.")
+    else:
+        messages.info(request, "You have already requested to join this group.")
+    return redirect('chipin:group_detail', group_id=group.id)
+
+@login_required
+def delete_join_request(request, request_id):
+    join_request = get_object_or_404(GroupJoinRequest, id=request_id, user=request.user)
+    # Ensure the logged-in user can only delete their own join requests
+    if join_request.user == request.user:
+        join_request.delete()
+        messages.success(request, "Your join request has been successfully deleted.")
+    else:
+        messages.error(request, "You are not authorised to delete this join request.")
+    return redirect('chipin:home')  
+    
+@login_required
+def leave_group(request, group_id):
+    group = get_object_or_404(Group, id=group_id)
+    # Check if the user is a member of the group
+    if request.user in group.members.all():
+        group.members.remove(request.user)  # Remove the user from the group
+        messages.success(request, f'You have left the group {group.name}.')
+    else:
+        messages.error(request, 'You are not a member of this group.') 
+    return redirect('chipin:home')
+
+@login_required
+def vote_on_join_request(request, group_id, request_id, vote):
+    group = get_object_or_404(Group, id=group_id)
+    join_request = get_object_or_404(GroupJoinRequest, id=request_id) 
+    if request.user not in group.members.all():
+        messages.error(request, "You must be a member of the group to vote.")
+        return redirect('chipin:group_detail', group_id=group.id)  
+    if request.user in join_request.votes.all():
+        messages.info(request, "You have already voted.")
+        return redirect('chipin:group_detail', group_id=group.id)
+        
+    # Register the user's vote
+    join_request.votes.add(request.user)
+    
+    # Calculate if more than 60% of members have approved
+    total_members = group.members.count()
+    total_votes = join_request.votes.count() 
+    if total_votes / total_members >= 0.6:
+        join_request.is_approved = True
+        group.members.add(join_request.user)  # Add the user to the group
+        join_request.save()
+        messages.success(request, f"{join_request.user.profile.nickname} has been approved to join the group!") 
+    return redirect('chipin:group_detail', group_id=group.id)
--- users/admin.py
+++ users/admin.py
@@ -3,7 +3,7 @@ from .models import Profile
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'first_name', 'surname', 'nickname')
-    search_fields = ('user__username', 'nickname')
+    search_fields = ('user_username', 'nickname')
     list_filter = ('nickname',)
-    
+
 admin.site.register(Profile, ProfileAdmin)
