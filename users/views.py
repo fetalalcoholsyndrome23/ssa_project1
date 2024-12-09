2b4079fe916ae94e6f108b81a8a6890842d8d716
Daniel Oh <danieloh@daniu.local>
2024-12-09 18:29:48 +1100

    Some QOL fixes and changes to website font

--- users/views.py
+++ users/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserRegistrationForm, UserTopUp
 from .models import Transcation
+import requests
+from django.conf import settings
 
 def register(request):
     if request.method == "POST":
@@ -18,35 +20,10 @@ def register(request):
         form = UserRegistrationForm()
     return render(request, 'users/register.html', {'form': form})
 
-@login_required(login_url='users:login')
-def user(request):
-    profile = request.user.profile
-    transactions = Transcation.objects.all().filter(user=request.user).order_by('-created_at')
-    return render(request, "users/user.html", {
-        'user': request.user,
-        'balance': profile.balance,
-        'transactions': transactions,
-    })
-
-def login_view(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            next_url = request.GET.get('next', reverse("users:user"))
-            return HttpResponseRedirect(next_url)
-        else:
-            messages.error(request, "Invalid Credentials.")
-    return render(request, "users/login.html")
-
 def logout_view(request):
     logout(request)
     messages.success(request, "Successfully logged out.")
     return redirect('users:login')
-import requests
-from django.conf import settings
 
 def login_view(request):
     if request.method == "POST":
@@ -97,3 +74,13 @@ def top_up(request):
     else:
         form = UserTopUp()
     return render(request, 'users/topup.html', {'form': form, 'balance': Profile.balance})
+
+@login_required(login_url='users:login')
+def user(request):
+    profile = request.user.profile
+    transactions = Transcation.objects.all().filter(user=request.user).order_by('-created_at')
+    return render(request, "users/user.html", {
+        'user': request.user,
+        'balance': profile.balance,
+        'transactions': transactions,
+    })
